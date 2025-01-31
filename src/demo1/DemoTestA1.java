2f578a0cf719813bc0374ae7acb49765694250ac
hayde <hayde@Hayder.attlocal.net>
2025-01-31 00:37:02 -0600

    adding

--- src/demo1/DemoTestA1.java
+++ src/demo1/DemoTestA1.java
@@ -2,11 +2,17 @@ package demo1;
 
 public class DemoTestA1 {
 	public static void main(String args []) {
+		System.out.println("noor");
 		
 		int a = 4;
 		int b = 4;
 		int result =a+b;
 		System.out.println(result);
+	}
+		public void add(int a,int b) {
+			System.out.println(a+b);
+			
+		}
 	}
 
 }
